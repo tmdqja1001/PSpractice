5afba5bf8c6731a2ac66786207c61c1800a511e6
Seungbeom <31874379+tmdqja1001@users.noreply.github.com>
2023-03-31 23:01:12 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    int flag = 1;
+    sort(citations.begin(), citations.end(), greater<int>());
+    for(int i=0 ; i<citations.size(); i++){
+        if(citations[i]>=i+1){
+            answer = i+1;
+        }
+        else break;
+    }
+    return answer;
+}
