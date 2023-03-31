459d9cde953a5ed7b4f7559e6bc83e8c3834d4a3
Seungbeom <31874379+tmdqja1001@users.noreply.github.com>
2023-04-01 01:09:20 +0900

    [level 3] Title: 등굣길, Time: 0.04 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
+    // n : 행, m : 열
+    int map[n][m];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            map[i][j] = 0;
+        }
+    }
+    for(auto p : puddles){
+        map[p[1]-1][p[0]-1] = -1;
+    }
+    for(int i=0; i<n; i++){ // i : 행
+        for(int j=0; j<m; j++){ // j : 열            
+            if(map[i][j] == -1){
+                map[i][j] = 0;
+            }
+            else if(j==0 && i==0){
+                map[i][j] = 1;
+            }
+            else if(i==0){
+                map[i][j] = map[i][j-1];
+            }
+            else if(j==0){
+                map[i][j] = map[i-1][j];
+            }
+            else{
+                map[i][j] = map[i-1][j] + map[i][j-1];
+                map[i][j] %= 1000000007;
+            }
+        }
+    }
+    return answer = map[n-1][m-1];
+}
