7997b193aeb00d432af47e9fc452a79a554d5bff
Seungbeom <31874379+tmdqja1001@users.noreply.github.com>
2023-04-19 13:44:53 +0900

    [Silver I] Title: 회의실 배정, Time: 92 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+vector<pair<int, int>> table;
+
+bool compare(pair<int, int> a, pair<int, int> b){
+    if(a.second != b.second) return a.second < b.second;
+    else return a.first < b.first;
+}
+
+int main(void){
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int s, e;
+        cin >> s >> e;
+        table.push_back({s, e});
+    }
+    sort(table.begin(), table.end(), compare);
+    int start = table[0].second;
+    int ret = 1;
+    for(int i=1; i<n; i++){
+        if(table[i].first >= start){
+            ret++;
+            start = table[i].second;
+        }
+    }
+    cout << ret;
+
+}
